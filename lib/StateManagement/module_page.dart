c8022d0c40a729b9e9c59b3e293d08e6e866d5b3
kevinWicaksono <kepinnatwork@gmail.com>
2024-07-30 00:31:15 +0700

    state management "drilling"

---
+++ lib/StateManagement/module_page.dart
@@ -0,0 +1,38 @@
+import 'package:dicoding_flutter/StateManagement/done_module_list.dart';
+import 'package:dicoding_flutter/StateManagement/module_list.dart';
+import 'package:flutter/material.dart';
+
+class ModulePage extends StatefulWidget {
+  const ModulePage({super.key});
+
+  @override
+  State<ModulePage> createState() => _ModulePageState();
+}
+
+class _ModulePageState extends State<ModulePage> {
+  final List<String> doneModuleList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Memulai Pemrograman Dengan Dart'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.done),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      DoneModuleList(doneModuleList: doneModuleList),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ModuleList(doneModuleList: doneModuleList),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'package:dicoding_flutter/NewsAppCupertino/Common/text_theme.dart';
-import 'package:dicoding_flutter/NewsAppCupertino/Data/Model/article.dart';
-import 'package:dicoding_flutter/NewsAppCupertino/UI/article_detail_page.dart';
-import 'package:dicoding_flutter/NewsAppCupertino/UI/article_web_view.dart';
-import 'package:dicoding_flutter/NewsAppCupertino/UI/home_page.dart';
-import 'package:dicoding_flutter/NewsAppCupertino/Common/styles.dart';
+import 'package:dicoding_flutter/StateManagement/module_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,38 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: Theme.of(context).colorScheme.copyWith(
-              primary: primaryColor,
-              secondary: secondaryColor,
-              onPrimary: Colors.black,
-            ),
-        textTheme: myTextTheme,
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        appBarTheme: const AppBarTheme(elevation: 0.8),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: secondaryColor,
-            foregroundColor: Colors.white,
-            textStyle: const TextStyle(),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(4),
-              ),
-            ),
-          ),
-        ),
-        useMaterial3: false,
       ),
-      initialRoute: HomePage.routeName,
-      routes: {
-        HomePage.routeName: (context) => const HomePage(),
-        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
-            article: ModalRoute.of(context)?.settings.arguments as Article),
-        ArticleWebView.routeName: (context) => ArticleWebView(
-            url: ModalRoute.of(context)?.settings.arguments as String),
-      },
+      home: const ModulePage(),
     );
   }
 }
